64266b5e8d42fb381077d5fd0612bda97ce3ff1c
Buzz <buzzherpop@gmail.com>
2017-04-24 19:40:37 -0400

    Reavel item by scrolling

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,5 @@
 import MobileMenu from './modules/MobileMenu'; //MobileMenu its var of the path
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
